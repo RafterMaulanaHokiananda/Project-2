e5b7e1fbc73c1161019934fe83e2311a8573e78e
RafterMaulanaHokiananda <42019738+RafterMaulanaHokiananda@users.noreply.github.com>
2020-03-24 23:56:16 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -42,6 +42,36 @@ app.post('/user',  (req, res) => {
     })
 })
 
+app.put('/user/:id',  (request, result) => {
+    let data = request.body
+    let sql =`
+        update user
+        set username = '`+data.username+`', password = '`+data.password+`'
+        where id_user = `+request.params.id+`
+    `
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err
+    })
+    result.json({
+        success: true,
+        message: 'Sipppp'
+    })
+})
+
+app.delete('/user/:id',  (request, result) => {
+    let sql =`
+        delete from persediaan where id_user = `+request.params.id+`
+    `
+
+    conn.query(sql, (err, res) => {
+        if(err) throw err
+    })
+    result.json({
+        success: true,
+        message: 'Data deleted'
+    })
+})
 
 //Persediaan
 app.get('/persediaan', (req, res) => {
@@ -59,7 +89,7 @@ app.get('/persediaan', (req, res) => {
 app.get('/persediaan/:id', (req, res) => {
     let sql = `
         select * from persediaan
-        where id_sedia = `+req.params.id+`
+        where id_barang = `+req.params.id+`
         limit 1
     `
 
@@ -89,12 +119,12 @@ app.post('/persediaan',  (req, res) => {
     })
 })
 
-app.put('/persediaan/:id',isAuthorized,  (request, result) => {
+app.put('/persediaan/:id',  (request, result) => {
     let data = request.body
     let sql =`
         update persediaan
         set nama = '`+data.nama+`', jenis = '`+data.jenis+`', harga = '`+data.harga+`, stok = '`+data.stok+`'
-        where id_sedia = `+request.params.id+`
+        where id_barang = `+request.params.id+`
     `
 
     conn.query(sql, (err, result) => {
@@ -108,7 +138,7 @@ app.put('/persediaan/:id',isAuthorized,  (request, result) => {
 
 app.delete('/persediaan/:id',  (request, result) => {
     let sql =`
-        delete from persediaan where id_sedia = `+request.params.id+`
+        delete from persediaan where id_barang = `+request.params.id+`
     `
 
     conn.query(sql, (err, res) => {
@@ -119,6 +149,31 @@ app.delete('/persediaan/:id',  (request, result) => {
         message: 'Data deleted'
     })
 })
+
+//Transaksi
+app.post('/pembelian/:id', (req, res) => {
+    let data = req.body
+
+    conn.query(`
+        insert into pesan (id_user, id_barang, jumlah)
+        values ('`+data.id_user+`', '`+req.params.id+`','`+data.jumlah+`')
+    `, (err, res) => {
+        if (err) throw err
+    })
+
+    conn.query(`
+        update lapangan
+        set status = stok - jumlah
+        where id_beli = '`+req.params.id+`'
+    `, (err, result) => {
+        if (err) throw err
+    })
+
+    res.json({
+        message: "Book has been taked by user"
+    })
+})
+
 
 
 //Transaksi
