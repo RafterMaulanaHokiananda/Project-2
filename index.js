c6814a9544d20e0b45018157e638862357ce2035
RafterMaulanaHokiananda <42019738+RafterMaulanaHokiananda@users.noreply.github.com>
2020-03-27 09:32:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const jwt = require('jsonwebtoken')
 
 
 //membuat aplikasi framework express
@@ -25,8 +26,60 @@ conn.connect((err) => {
     if(err) throw err
     console.log('Connected............')
 })
+
+
+const isAuthorized = (req, res, next) => {
+    if(typeof(req.headers['x-api-key']) == 'undefined')
+    {
+        return res.status(403).json({
+            success: false,
+            message: 'Unathorized Token is not provided'
+        })
+    }
+
+
+//get token dari headers
+let token = req.headers['x-api-key']
+
+//melakukuan verivikasi token yang dikirim user
+jwt.verify(token, secretKey, (err, decoded) => {
+    if(err)
+    {
+        return res.status(403).json({
+            success: false,
+            message: 'Unathorized Token'
+        })
+    }
+})
+
+//lanjut ke next request
+next()
+}
+
+
+//Login
+
+app.post('/login', (req, res) => {
+    let data = req.body
+
+    if(data.username == 'admin' && data.password == 'admin')
+    {
+        let token = jwt.sign(data.username + ' | ' + data.password, secretKey)
+
+        res.json({
+            success: true,
+            message: 'Sukses Login',
+            token: token
+        })
+    }
+    res.json({
+        success: false,
+        message: 'dohhh'
+    })
+})
+
 //User
-app.post('/user',  (req, res) => {
+app.post('/user',isAuthorized,  (req, res) => {
     let data = req.body
     let sql = `
         insert into user(username, password)
@@ -42,7 +95,7 @@ app.post('/user',  (req, res) => {
     })
 })
 
-app.put('/user/:id',  (request, result) => {
+app.put('/user/:id',isAuthorized,  (request, result) => {
     let data = request.body
     let sql =`
         update user
@@ -59,7 +112,7 @@ app.put('/user/:id',  (request, result) => {
     })
 })
 
-app.delete('/user/:id',  (request, result) => {
+app.delete('/user/:id',isAuthorized,  (request, result) => {
     let sql =`
         delete from persediaan where id_user = `+request.params.id+`
     `
@@ -74,7 +127,7 @@ app.delete('/user/:id',  (request, result) => {
 })
 
 //Persediaan
-app.get('/persediaan', (req, res) => {
+app.get('/persediaan',isAuthorized, (req, res) => {
     let sql = 'select * from persediaan'
     conn.query(sql, (err, result) => {
         if(err) throw err
@@ -86,7 +139,7 @@ app.get('/persediaan', (req, res) => {
     })
 })
 
-app.get('/persediaan/:id', (req, res) => {
+app.get('/persediaan/:id',isAuthorized, (req, res) => {
     let sql = `
         select * from persediaan
         where id_barang = `+req.params.id+`
@@ -103,10 +156,10 @@ app.get('/persediaan/:id', (req, res) => {
     })
 })
 
-app.post('/persediaan',  (req, res) => {
+app.post('/persediaan',isAuthorized,  (req, res) => {
     let data = req.body
     let sql = `
-        insert into data(nama, jenis,  harga, stok)
+        insert into persediaan(nama, jenis, harga, stok)
         values('`+data.nama+`','`+data.jenis+`','`+data.harga+`','`+data.stok+`');
     `
 
@@ -119,24 +172,26 @@ app.post('/persediaan',  (req, res) => {
     })
 })
 
-app.put('/persediaan/:id',  (request, result) => {
-    let data = request.body
-    let sql =`
+app.put('/persediaan/:id',isAuthorized, (req, res) => {
+    let data = req.body
+
+    let sql = `
         update persediaan
-        set nama = '`+data.nama+`', jenis = '`+data.jenis+`', harga = '`+data.harga+`, stok = '`+data.stok+`'
-        where id_barang = `+request.params.id+`
+        set nama = '`+data.nama+`', jenis = '`+data.jenis+`', harga = '`+data.harga+`', stok = '`+data.stok+`'
+        where id_barang = '`+req.params.id+`'
     `
 
     conn.query(sql, (err, result) => {
-        if(err) throw err
-    })
-    result.json({
-        success: true,
-        message: 'Sipppp'
+        if (err) throw err
+
+        res.json({
+            message: "data has been updated",
+            data: result[0]
+        })
     })
 })
 
-app.delete('/persediaan/:id',  (request, result) => {
+app.delete('/persediaan/:id',isAuthorized,  (request, result) => {
     let sql =`
         delete from persediaan where id_barang = `+request.params.id+`
     `
@@ -151,30 +206,63 @@ app.delete('/persediaan/:id',  (request, result) => {
 })
 
 //Transaksi
-app.post('/pembelian/:id', (req, res) => {
+app.post('/pembelian/:id',isAuthorized, (req, res) => {
     let data = req.body
 
     conn.query(`
-        insert into pesan (id_user, id_barang, jumlah)
-        values ('`+data.id_user+`', '`+req.params.id+`','`+data.jumlah+`')
+        insert into pembelian (id_user, id_barang, jumlah,id_bayar)
+        values ('`+data.id_user+`', '`+req.params.id+`','`+data.jumlah+`','`+data.id_bayar+`')
     `, (err, res) => {
         if (err) throw err
     })
 
     conn.query(`
-        update lapangan
-        set status = stok - jumlah
-        where id_beli = '`+req.params.id+`'
+        update persediaan
+        set stok = stok - '`+data.jumlah+`'
+        where id_barang = '`+req.params.id+`'
     `, (err, result) => {
         if (err) throw err
     })
 
     res.json({
-        message: "Book has been taked by user"
+        message: "Pembelian berhasil"
     })
 })
 
+app.put('/pembelian/:id',isAuthorized, (req, res) => {
+    let data = req.body
 
+    let sql = `
+        update pembelian
+        set id_user = '`+data.id_user+`', id_barang = '`+data.id_barang+`', jumlah = '`+data.jumlah+`', id_bayar = '`+data.id_bayar+`'
+        where id_beli = '`+req.params.id+`'
+    `
+
+    conn.query(sql, (err, result) => {
+        if (err) throw err
+
+        res.json({
+            message: "data has been updated",
+            data: result
+        })
+    })
+})
+
+app.delete('/pembelian/:id',isAuthorized, (req, res) => {
+    let sql = `
+        delete from pembelian
+        where id_beli = '`+req.params.id+`'
+    `
+
+    conn.query(sql, (err, result) => {
+        if (err) throw err
+        
+        res.json({
+            message: "data has been deleted",
+            data: result
+        })
+    })
+})
 
 //Transaksi
 
